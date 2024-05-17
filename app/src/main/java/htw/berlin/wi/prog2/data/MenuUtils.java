70cc86fc38a43950a486c1d5624cd4c5c6bca450
LuisRamos1311 <s0555373@htw-berlin.de>
2024-05-17 22:02:14 +0200

    Finished focusOnNameAndInvert... created a new HashMap and inverted the values and keys.

--- app/src/main/java/htw/berlin/wi/prog2/data/MenuUtils.java
+++ app/src/main/java/htw/berlin/wi/prog2/data/MenuUtils.java
@@ -2,20 +2,28 @@ package htw.berlin.wi.prog2.data;
 
 import htw.berlin.wi.prog2.domain.Ingredient;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MenuUtils {
 
     public static List<String> focusOnNames(Map<Long, Ingredient> articles) {
         List<String> names = new ArrayList<>();
-        for (Ingredient art : articles.values()) names.add(art.getName());
+        for (Ingredient art : articles.values()) {
+            names.add(art.getName());
+        }
         return names;
     }
 
     public static Map<String, Long> focusOnNameAndInvert(Map<Long, Ingredient> articles) {
-        return Map.of(); // TODO hier implementieren und korrekte Ergebnis-Map zurückgeben
+        Map<String, Long> map = new HashMap<>();
+
+        for(Map.Entry<Long,Ingredient> art : articles.entrySet()) {
+           
+            map.put(art.getValue().getName(), art.getKey());
+        }
+
+
+        return map; // TODO hier implementieren und korrekte Ergebnis-Map zurückgeben
     }
 
     public static List<Ingredient> ingredientsFromIdAndCount(Map<Long, Integer> idsAndCount, Map<Long, Ingredient> articles) {
